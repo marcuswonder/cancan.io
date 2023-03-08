db6d1149ffd93d3c496798ff5c602de9c6a9e225
marcuswonder <marcus.bruce.smith@gmail.com>
2023-03-08 11:56:20 -0800

    Board model refactored to include bigSteps and babySteps, with BigStep model deleted. Create Board function working.

--- models/board.js
+++ models/board.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const boardSchema = new Schema({
+
+const babyStepSchema = new Schema({
     title: {type: String, required: true},
-    description: {type: String},
+    description: {type: String, required: true},
+    due: {type: Date},
     status: {
         type: String,
         enum: ['Planned', 'In Progress', 'Complete'],
@@ -15,10 +17,53 @@ const boardSchema = new Schema({
         ref: 'User',
         required: true,
     },
+    responsible: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+    }}, {
+        timestamps: true,
+    
+})
+
+const bigStepSchema = new Schema({
+    title: {type: String, required: true},
+    description: {type: String, required: true},
+    due: {type: Date},
+    status: {
+        type: String,
+        enum: ['Planned', 'In Progress', 'Complete'],
+        default: 'Planned',
+        required: true
+    },
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    responsible: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    babySteps: [babyStepSchema]
+}, {
+        timestamps: true,
+    
+})
+
+
+const boardSchema = new Schema({
+    title: {type: String, required: true},
+    description: {type: String},
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
     users: [{
         type: Schema.Types.ObjectId,
         ref: 'User',
     }],
+    bigSteps: [bigStepSchema]
 }, {
         timestamps: true,
     
@@ -28,5 +73,16 @@ boardSchema.statics.findByTitle = async function(title) {
     const board = await this.findOne({ title });
     return board;
   };
+
+
+boardSchema.methods.addUsersToBoard = function() {
+    const babyStepUsers = this.bigSteps.babySteps.find({}).populate({ path: 'responsible', model: 'User' })
+    console.log("babyStepUsers", babyStepUsers)
+    const bigStepUsers = this.bigSteps.find({}).populate({ path: 'responsible', model: 'User' })
+    console.log("bigStepUsers", bigStepUsers)
+    const boardUsers = [...babyStepUsers, bigStepUsers]
+    this.users.push(boardUsers)
+    console.log("boardUsers", boardUsers)
+  }
 
 module.exports = mongoose.model('Board', boardSchema);
