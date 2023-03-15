60a349d28b9abf8bfe7f22ba6e4b83f2143ec5c1
marcuswonder <marcus.bruce.smith@gmail.com>
2023-03-14 18:43:21 -0700

    UserBoardLists using same format as AuthorBoardList

--- src/components/UserBoardsList/UserBoardsList.jsx
+++ src/components/UserBoardsList/UserBoardsList.jsx
@@ -1,72 +1,47 @@
-import '../AuthorBoardsList/AuthorBoardsList.css'
+import './UserBoardsList.css'
 import detailsIcon from '../../public/assets/details-white.png'
+import deleteIcon from '../../public/assets/delete-icon-white.png'
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import * as boardsAPI from '../../utilities/boards-api'
+import BoardCard from '../BoardCard/BoardCard'
 
-export default function UserBoardsList() {
+export default function UserBoardsList({ user }) {
     const [userBoardGallery, setUserBoardGallery] = useState([])
+    const [hoveredTitle, setHoveredTitle] = useState(null);
+    const [hoveredDescription, setHoveredDescription] = useState(null);
+    
 
     useEffect(function() {
         async function getUserBoards() {
-          const boards = await boardsAPI.getUserBoards()
-          setUserBoardGallery(boards)
+          const userBoardGallery = await boardsAPI.getUserBoards()
+          setUserBoardGallery(userBoardGallery)
         }
         getUserBoards()
     }, [])
 
-
+    
     return (
-        <>
+    <>
         {userBoardGallery.length ?
             <div>
                 <h2 className="boards-list-h2">Collaborative Boards</h2>
-                <p className="boards-list-custom-p">These are boards that you are a user of</p>
-                    <div className="boards-list-board-body">
-                        {userBoardGallery.map(board => (
-                            <div className="boards-list-board-card-container" key={board._id}>
-                                <div className="boards-list-board-card">
-                                    <div className="boards-list-board-navigation">
-                                        <div className="boards-list-board-navigation-details-container">
-                                            <Link to={`/boards/${board.title.replace(/\s+/g, '-')}`} key={board._id}><img className="boards-list-boards-details-icon" src={detailsIcon} alt='See a detailed view of your board'  title="See a detailed view of your board" /></Link>
-                                        </div>
-                                        <div className="boards-list-board-navigation-delete-container">
-                                            <Link to={`/boards/${board.title.replace(/\s+/g, '-')}`} key={board._id}></Link>
-                                        </div>
-                                    </div>
-                                    <h2 className="boards-list-board-card-title">{board.title}</h2>
-                                    <p className="boards-list-board-card-description">{board.description}</p>
-                                    <p className="boards-list-board-card-description">Big Step Summary</p>
-                                    <div className="boards-list-table-container">
-                                        <table className="boards-list-table">
-                                            <thead>
-                                                <tr>
-                                                    <th className="boards-list-th">Planned</th>
-                                                    <th className="boards-list-th">In Progress</th>
-                                                    <th className="boards-list-th">Complete</th>
-                                                    <th className="boards-list-th">% Complete</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                <tr>
-                                                    <td className="boards-list-td">{board.plannedBigStepsCount}</td>
-                                                    <td className="boards-list-td">{board.inProgressBigStepsCount}</td>
-                                                    <td className="boards-list-td">{board.completeBigStepsCount}</td>
-                                                    <td className="boards-list-td">{board.bigStepCompletionRate}</td>
-                                                </tr>
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                
-                                </div>
-                            </div>
-                        ))}
-                    </div>
+                <p className="boards-list-custom-p">These are boards that you are a user on</p>
+                <div className="boards-list-board-body">
+                    {userBoardGallery.map(board => (
+                    <BoardCard key={board._id} board={board} />
+                    ))}
+                </div>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
             </div>
-
         :
-        <></>
-        }
+        <>
         </>
-    )
+        }
+
+    </>
+    );
 }
