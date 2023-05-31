fe8466558e86746a724acb73268f2e30889a6554
marcuswonder <marcus.bruce.smith@gmail.com>
2023-05-31 09:41:15 +0100

    Signed Up user redirects to board page

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -42,7 +42,7 @@ export default function SignUpForm(props) {
       };
       const user = await signUp(formData);
       props.setUser(user);
-      navigate('/');
+      navigate('/boards');
     } catch {
       setError('Sign Up Failed - Try Again');
     }
@@ -72,74 +72,3 @@ export default function SignUpForm(props) {
     </div>
   );
 }
-
-
-
-
-
-
-
-
-
-
-
-// import './SignUpForm.css'
-// import { Component } from 'react'
-// import { signUp } from '../../utilities/users-service'
-
-// export default class SignUpForm extends Component {
-//   state = {
-//     name: '',
-//     email: '',
-//     password: '',
-//     confirm: '',
-//     error: '',
-//   }
-  
-//   handleChange = (evt) => {
-//       this.setState({
-//           [evt.target.name]: evt.target.value,
-//           error: ''
-//       })
-//   }
-
-//   handleSubmit = async (evt) => {
-//       evt.preventDefault()
-//       try {
-//         const formData = {
-//           name: this.state.name,
-//           email: this.state.email,
-//           password: this.state.password
-//         }
-//         const user = await signUp(formData)
-//         this.props.setUser(user)
-        
-//       } catch {
-//         this.setState({ error: 'Sign Up Failed - Try Again' })
-//       }
-//   } 
-
-//   render() {
-//     const disable = this.state.password !== this.state.confirm
-
-//     return (
-//       <div>
-//           <div className="form-container">
-//           <h1 className="sign-in-h1">yes you cancan!</h1>
-//             <form autoComplete="off" onSubmit={this.handleSubmit}>
-//               <label>Name</label>
-//               <input type="text" name="name" value={this.state.name} onChange={this.handleChange} required />
-//               <label>Email</label>
-//               <input type="email" name="email" value={this.state.email} onChange={this.handleChange} required />
-//               <label>Password</label>
-//               <input type="password" name="password" value={this.state.password} onChange={this.handleChange} required />
-//               <label>Confirm</label>
-//               <input type="password" name="confirm" value={this.state.confirm} onChange={this.handleChange} required />
-//               <button type="submit" disabled={disable}>SIGN UP</button>
-//             </form>
-//           </div>
-//           <p className="error-message">&nbsp{this.state.error}</p>
-//       </div>
-//     )
-//   }
-// }
